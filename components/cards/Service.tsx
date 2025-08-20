272af2a4b22fafb016bb6b97c531d09a97a8156d
zyadfallatah <zyadfallatah@gmail.com>
2025-08-20 17:21:24 +0300

    Remove: links from features in home

--- components/cards/Service.tsx
+++ components/cards/Service.tsx
@@ -4,23 +4,18 @@ import React from "react";
 interface Props {
   title: string;
   description: string;
+  link: string;
 }
 
-const Service = ({ title, description }: Props) => {
+const Service = ({ title, description, link }: Props) => {
   return (
-    <div className="flex flex-col items-center justify-center text-center gap-4 sm:gap-6 bg-white shadow shadow-white p-4 sm:p-6 w-full lg:max-w-[380px] min-h-[220px] sm:min-h-[320px] lg:min-h-[350px] rounded-[11px]">
+    <div className="flex flex-col items-center justify-center text-center gap-4 sm:gap-6 bg-white shadow shadow-white p-4 sm:p-6 w-full lg:max-w-[380px] min-h-[220px] sm:min-h-[320px] lg:min-h-[250px] rounded-[11px]">
       <h3 className="text-xl sm:text-2xl lg:text-3xl font-bold text-primary">
         {title}
       </h3>
       <p className="text-base sm:text-lg lg:text-xl leading-relaxed">
         {description}
       </p>
-      <Link
-        href="#"
-        className="bg-primary text-white w-full lg:max-w-[250px] p-2 sm:p-3 rounded-sm text-sm sm:text-base hover:bg-primary-dark transition-colors"
-      >
-        اذهب
-      </Link>
     </div>
   );
 };
